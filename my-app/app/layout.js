0a204ebd360de52afe5c2ec11b1175424fadb5d6
Shyam <shyambhattarai057@gmail.com>
2026-08-26 10:24:36 +0545

    css added

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="font-poppins antialiased">{children}</body>
     </html>
   );
 }
